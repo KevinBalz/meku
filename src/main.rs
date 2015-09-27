5b1dc37105d869400dc7a83c3fe536640b073fa4
Kevin Balz <kevin.balz@web.de>
2015-09-27 10:26:07 +0200

    added map_dir function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::fs;
+use std::env;
+use std::path::Path;
 
 fn main() {
-    
+    for dir in env::args().skip(1){
+        map_dir(dir,|path| println!("{:?}",path));
+    }
+}
+
+fn map_dir<P: AsRef<Path>,F: Fn(&Path)>(dirref: P,func: F) {
+    let dir = dirref.as_ref();
+    for entry in fs::read_dir(dir).ok().unwrap() {
+        let dir = entry.ok().unwrap();
+        func(&dir.path());
+    }
 }
