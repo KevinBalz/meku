28d7aa778c63638d8c2fefad8d65682a23400db1
Kevin Balz <kevin.balz@web.de>
2015-10-01 10:10:34 +0200

    Added src and target directory cli options

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ fn main() {
         .author("Kevin Balz")
         .version("not nearly finished")
         .about("An awesome build system for making things.")
+        .arg(Arg::with_name("source")
+            .short("C")
+            .long("directory")
+            .takes_value(true)
+            .help("Sets the source directory (working directory by default)")
+        )
         .arg(Arg::with_name("output")
             .index(1)
             .required(true)
@@ -17,8 +23,13 @@ fn main() {
             .help("Sets the output folder/s")
         )
         .get_matches();
-    for dir in args.values_of("output").unwrap().iter() {
-        map_contents(dir,&|path| println!("{:?}",path));
+
+    let working_dir = env::current_dir().unwrap();
+    let src_dir = args.value_of("source").map(Path::new).unwrap_or(working_dir.as_path());
+
+    map_contents(src_dir,&|path| println!("{:?}",path));
+    for tar_dir in args.values_of("output").unwrap().iter() {
+        println!("OUTPUT FOLDER: {}",tar_dir);
     }
 }
 
