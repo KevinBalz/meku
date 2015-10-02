c4275c7fd17a33f59b51b80dd605331adc8b3058
Kevin Balz <kevin.balz@web.de>
2015-10-02 20:17:47 +0200

    Better commandline help and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 extern crate clap;
 extern crate meku;
 
-use std::fs;
 use std::env;
-use std::path;
 use std::path::Path;
 use clap::{Arg, App, SubCommand};
 
 fn main() {
-    let args = App::new("myapp")
+    let args = App::new("meku")
         .author("Kevin Balz")
-        .version("not nearly finished")
-        .about("An awesome build system for making things.")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("an awesome build system for game makers")
         .arg_required_else_help(true)
         .arg(Arg::with_name("source")
             .short("C")
@@ -20,6 +18,7 @@ fn main() {
             .help("Sets the source directory (working directory by default)")
         )
         .subcommand(SubCommand::with_name("build")
+            .about("Builds the source folder to the given directory/directories")
             .arg(Arg::with_name("output")
                 .index(1)
                 .required(true)
@@ -28,6 +27,7 @@ fn main() {
             )
         )
         .subcommand(SubCommand::with_name("run")
+            .about("Builds the source folder and then runs a command")
             .arg(Arg::with_name("cmd")
                 .index(1)
                 .required(true)
@@ -53,6 +53,5 @@ fn main() {
         },
         _ => ()
     };
-
 
 }
